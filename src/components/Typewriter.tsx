0cee3195d5a421cfd36598b2e764f02c49abf30f
Davide Maggi <davide.maggi@live.com>
2023-08-31 21:44:55 +0200

    New Icon

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 type TypewriterProps = {
-    prefix: string,
+    prefix: string[],
     text: string
   }
 
 const Typewriter = ({text,prefix}:TypewriterProps) => {
   const [displayText, setDisplayText] = useState('');
   const typingSpeed = 50; // Adjust this value to control typing speed
+const prefixShow: string | null = getRandomItemFromArray(prefix); 
 
   useEffect(() => {
     console.log(text);
@@ -32,7 +33,16 @@ const Typewriter = ({text,prefix}:TypewriterProps) => {
     };
   }, [text]);
 
-  return <>{prefix}&nbsp;{displayText}_<div id="cursor"></div></>;
+  return <>{prefixShow}&nbsp;{displayText}_<div id="cursor"></div></>;
 };
+
+function getRandomItemFromArray(array: string[]): string | null {
+  if (array.length === 0) {
+    return null; // Return null if the array is empty
+  }
+
+  const randomIndex: number = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
 
 export default Typewriter;
